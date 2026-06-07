0cccea729a333dbb30780f9671fb0209e7e9a35f
BigMike <dev@flynn.network>
2026-06-06 18:52:17 -0700

    Remove qemu ananicy rule

--- modules/services/ananicy.nix
+++ modules/services/ananicy.nix
@@ -9,11 +9,5 @@
       check_freq = 1;
       apply_cgroup = false;
     };
-    extraTypes = [
-      { type = "Custom_VM"; nice = -20; ioclass = "realtime"; latency_nice = -20; }
-    ];
-    extraRules = [
-      { name = "qemu-system-x86_64"; type = "Custom_VM"; }
-    ];
   };
 }
